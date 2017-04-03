c7dba6b674ec640159d9acb55e69ddd2846db0ad
wys12 <595068001@qq.com>
2017-04-03 09:50:01 +0800

    公司查找模板

--- taokefu/src/main/java/com/yc/taokefu/web/handler/CompanyHandler.java
+++ taokefu/src/main/java/com/yc/taokefu/web/handler/CompanyHandler.java
@@ -1,5 +1,25 @@
 package com.yc.taokefu.web.handler;
 
-public class CompanyHandler {
+import org.apache.logging.log4j.LogManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.yc.taokefu.entity.Company;
+import com.yc.taokefu.service.CompanyService;
+
+@Controller("companyHandler")
+@RequestMapping("company")
+public class CompanyHandler {
+	@Autowired
+	private CompanyService companyService;
+	
+	@RequestMapping(value="findCompany",method=RequestMethod.POST)
+	@ResponseBody
+	public Company findCompany(Company company) {
+		LogManager.getLogger().debug("查找公司   === " +company);
+		return company;
+	}
 }
