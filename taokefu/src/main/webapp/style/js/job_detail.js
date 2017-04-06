63290753fab542d9e22e8dee452b4136428e77d7
wys12 <595068001@qq.com>
2017-04-06 20:34:33 +0800

    js错误

--- taokefu/src/main/webapp/style/js/job_detail.js
+++ taokefu/src/main/webapp/style/js/job_detail.js
@@ -1,4 +1,4 @@
-<script src="js/toudi.js" type="text/javascript"></script>jQuery.noConflict(true);
+jQuery.noConflict(true);
 $(function(){
 	$('.popup .filePrew').hover(function(){
 		$(this).parent('.btn_addPic').addClass('btn_addPic_hover');
